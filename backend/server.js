cf573070c5be9f4c61784d56df4d1ce92f4b5173
Chaithanya <krishnachaithanya826@gmail.com>
2024-08-31 16:50:41 +0530

    minor refactor in folder backend.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("reflect-metadata")
 require("dotenv").config()
 const PORT = process.env.PORT
-const app = require("./app")
+const app = require("./src/app")
 
 app.listen(PORT, () => console.log(`Express server running on port ${PORT}.`))
